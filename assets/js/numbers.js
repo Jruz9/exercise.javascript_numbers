098504521e51e9c3adc5c964bc7046811ebb4536
Jose Cruz <josec300@live.com>
2020-07-01 08:10:05 -0400

    A lot of work to do pushing just in case

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,3 +1,10 @@
 function compute(expression) {
     // TODO - write method definition here
+    var final=0;
+    var q=expression;
+     var p=new Array;
+
+     // this function seperates the expression into arrays
+     
+
 }
